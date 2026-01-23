5e8c2c710e7039fe4d1a9b383ce71fd0a1dceedf
Duncan Giles <duncan3142@gmail.com>
2026-01-23 19:36:53 +0000

    build: mise tasks

--- tasks/format.sh
+++ tasks/format.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-#MISE description "Formats the code"
-#USAGE flag "--check" help="Check if files are formatted"
+#MISE description "Format source code"
+#USAGE arg "[mode]" help="Format mode" default="check" {
+#USAGE   choices "check" "write"
+#USAGE }
 
 set -ueC
 set -o pipefail
 
-if [[ "${usage_check:-false}" == "true" ]]; then
-  flag="--check"
-else
-  flag="--write"
-fi
-
-npm exec -- prettier "$flag" --cache --cache-location='.prettier/cache' --cache-strategy content .
+npm exec -- prettier "--${usage_mode?}" --cache --cache-location='.prettier/cache' --cache-strategy content .
