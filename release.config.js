b267e2fa203938af254b162de5965c1e01ed7b8d
Duncan Giles <duncan3142@gmail.com>
2026-01-31 22:22:53 +0000

    ci: release comment

--- release.config.js
+++ release.config.js
@@ -5,6 +5,11 @@ const commitPartial = readFileSync(
 	"utf8"
 )
 
+const pkg = JSON.parse(readFileSync(new URL("./package.json", import.meta.url), "utf8"))
+const repoRegex = /^git\+https:\/\/github\.com\/([a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)\.git$/
+const [, repoPath] = repoRegex.exec(pkg.repository.url)
+const [, pkgName] = pkg.name.split("/")
+
 export default {
 	branches: ["main"],
 	plugins: [
@@ -32,6 +37,16 @@ export default {
 				message: "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
 			},
 		],
-		"@semantic-release/github",
+		[
+			"@semantic-release/github",
+			{
+				successComment:
+					":tada: This PR is included in version ${nextRelease.version} :tada:\n\nThe release is available on:\n\n- [GitHub release](${releases.find(r => r.name === 'github').url})\n- [GitHub package](https://github.com/" +
+					repoPath +
+					"/pkgs/npm/" +
+					pkgName +
+					")",
+			},
+		],
 	],
 }
