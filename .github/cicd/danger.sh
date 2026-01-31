9f4ff863b151296a4a89d6a15bd1da5388eb2117
Duncan Giles <duncan3142@gmail.com>
2026-01-31 21:05:05 +0000

    ci: esm dangerfile

--- .github/cicd/danger.sh
+++ .github/cicd/danger.sh
@@ -8,6 +8,7 @@ set -o pipefail
 npm install --no-save \
   @commitlint/lint@20 \
   @commitlint/load@20 \
+  danger@13 \
   remark@15
 
 danger ci --fail-on-warnings=true --dangerfile .github/cicd/dangerfile.mjs
